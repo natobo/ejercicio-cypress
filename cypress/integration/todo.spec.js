4a8922eb243c5b785ff86486f084e638e497f766
Nicolás Tobo <na.tobo@uniandes.edu.co>
2021-11-06 23:51:55 -0500

    ejercicio completo

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -21,4 +21,21 @@ describe('TODOS page', () => {
       .click()
       .should('have.class', 'complete');
   });
+  // TODO DELETE VALIDATION ITEM EXERCISE
+  // First it creates an item and after that it deletes the item
+  it('should delete a TODO', () => {
+    cy.visit('todo')
+      .get('input')
+      .type('new TODO')
+      .get('button[type=submit]')
+      .click()
+      .get('.list-group > .list-group-item:first > p')
+      .first()
+      .should('contain', '1. new TODO')
+      .get('.list-group > .list-group-item:first > button')
+      .click()
+      .get('.list-group')
+      .find('.list-group-item')
+      .should('have.length', 0)
+  });
 });
